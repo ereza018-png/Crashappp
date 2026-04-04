7cff935bcc4d64ef2b9b46e541f4669c67b48b7d
ereza018-png <ereza018@gmail.com>
2026-04-04 01:12:10 -0600

    Update Android permissions in App.js
    Removed unnecessary permissions from the Android configuration.

--- App.js
+++ App.js
@@ -3,15 +3,11 @@
     "name": "AppCrashNueva",
     "slug": "app-crash-nueva",
     "version": "1.0.0",
-    "platforms": ["android"],
     "android": {
       "package": "com.ereza018.appcrashnueva",
       "permissions": [
         "ACCESS_FINE_LOCATION",
-        "ACCESS_COARSE_LOCATION",
-        "ACCESS_BACKGROUND_LOCATION",
         "CAMERA",
-        "RECORD_AUDIO",
         "READ_EXTERNAL_STORAGE",
         "WRITE_EXTERNAL_STORAGE"
       ]
