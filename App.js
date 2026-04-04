8c302f3855511fed98979ee25ac914ad970d476d
ereza018-png <ereza018@gmail.com>
2026-04-04 01:19:10 -0600

    Simplify Android permissions in App.js
    Removed unnecessary permissions from the Android configuration.

--- App.js
+++ App.js
@@ -4,13 +4,7 @@
     "slug": "app-crash-nueva",
     "version": "1.0.0",
     "android": {
-      "package": "com.ereza018.appcrashnueva",
-      "permissions": [
-        "ACCESS_FINE_LOCATION",
-        "CAMERA",
-        "READ_EXTERNAL_STORAGE",
-        "WRITE_EXTERNAL_STORAGE"
-      ]
+      "package": "com.ereza018.appcrashnueva"
     }
   }
 }
